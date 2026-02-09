ba7236743b36081f80a9e073f4cfa7e4f38eada2
MammouthRJ <159185225+MammouthRJ@users.noreply.github.com>
2026-02-09 00:39:56 -0300

    fonction delete

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from .database import Base, engine, SessionLocal
 from . import models
 from sqlalchemy.orm import Session
-from .schemas   import FilmCreate
+from .schemas   import FilmCreate, FilmRead
+from typing import List
 
 
 Base.metadata.create_all(bind=engine)
@@ -22,7 +23,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/films")
+@app.post("/films", response_model=FilmRead)
 def create_film(film: FilmCreate, db: Session = Depends(get_db)):
     db_film = models.Film(
         title=film.title,
@@ -33,6 +34,17 @@ def create_film(film: FilmCreate, db: Session = Depends(get_db)):
     db.refresh(db_film)
     return db_film
 
-@app.get("/films")
+@app.get("/films", response_model=List[FilmRead])
 def get_films(db: Session = Depends(get_db)):
     return db.query(models.Film).all()
+
+@app.delete("/films/{film_id}")
+def delete_film(film_id: int, db: Session = Depends(get_db)):
+    film = db.query(models.Film).filter(models.Film.id == film_id).first()
+
+    if film is None:
+        raise HTTPException(status_code=404, detail="Film not found")
+
+    db.delete(film)
+    db.commit()
+    return {"ok": True, "deleted_id": film_id}
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class FilmCreate(BaseModel):
+    title: str
+    director: str
+class FilmRead(BaseModel):
+    model_config = ConfigDict(from_attributes=True)  # Pydantic v2
+    id: int
     title: str
     director: str
